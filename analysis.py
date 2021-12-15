db1fb917ead0f4a257ff4e063e5d1bfb2d1786e7
AstroJackson <jacksondtaylor4@gmail.com>
2021-12-15 16:44:13 -0500

    mac

---
+++ analysis.py
@@ -0,0 +1,26 @@
+import numpy as np, matplotlib.pyplot as plt, matplotlib as mpl, os
+import pydrive
+
+absolute_initial_path = '/Users/jackson/Desktop/Rebound/ReboundResonances'
+top_path = "Desktop"
+next_path = ''
+
+os.chdir(absolute_initial_path)
+#####################
+while os.getcwd().split('/')[-1] != top_path:
+    os.chdir("..")
+
+for i in os.listdir():
+    continue
+    #print(i)
+
+######################
+os.chdir(absolute_initial_path)
+
+
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
+
+gauth = GoogleAuth()
+gauth.LocalWebserverAuth() # client_secrets.json need to be in the same directory as the script
+# drive = GoogleDrive(gauth)
