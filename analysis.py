a30c85cf22dc6b224450a0d56a131b4f33c6d3b7
AstroJackson <jacksonisboss1@gmail.com>
2022-02-14 14:37:10 -0500

    sunny

--- analysis.py
+++ analysis.py
@@ -1,26 +1,27 @@
-import numpy as np, matplotlib.pyplot as plt, matplotlib as mpl, os
-import pydrive
+# import numpy as np, matplotlib.pyplot as plt, matplotlib as mpl, os
+# import pydrive
 
-absolute_initial_path = '/Users/jackson/Desktop/Rebound/ReboundResonances'
-top_path = "Desktop"
-next_path = ''
+# absolute_initial_path = '/Users/jackson/Desktop/Rebound/ReboundResonances'
+# top_path = "Desktop"
+# next_path = ''
 
-os.chdir(absolute_initial_path)
-#####################
-while os.getcwd().split('/')[-1] != top_path:
-    os.chdir("..")
+# os.chdir(absolute_initial_path)
+# #####################
+# while os.getcwd().split('/')[-1] != top_path:
+#     os.chdir("..")
 
-for i in os.listdir():
-    continue
-    #print(i)
+# for i in os.listdir():
+#     continue
+#     #print(i)
 
-######################
-os.chdir(absolute_initial_path)
+# ######################
+# os.chdir(absolute_initial_path)
 
 
-from pydrive.auth import GoogleAuth
-from pydrive.drive import GoogleDrive
+# from pydrive.auth import GoogleAuth
+# from pydrive.drive import GoogleDrive
 
-gauth = GoogleAuth()
-gauth.LocalWebserverAuth() # client_secrets.json need to be in the same directory as the script
-# drive = GoogleDrive(gauth)
+# gauth = GoogleAuth()
+# gauth.LocalWebserverAuth() # client_secrets.json need to be in the same directory as the script
+# # drive = GoogleDrive(gauth)
+import rebound
